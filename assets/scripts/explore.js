c930c3f874d35b0706c2a5e3a73c3a85ef908475
Arnav710 <mailto.arnav.modi@gmail.com>
2024-05-03 19:32:46 -0700

    speak typed text based on selected voice

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,47 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+let voices = [];
+
+function populateVoiceList() {
+  voices = speechSynthesis.getVoices();
+
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      option.textContent += " — DEFAULT";
+    }
+
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    document.getElementById("voice-select").appendChild(option);
+  }
+}
+
+function speak(event){
+  let textarea = document.querySelector('textarea');
+  let input_text = textarea.value;
+
+  let utterance = new SpeechSynthesisUtterance(input_text);
+  const voiceSelect = document.getElementById("voice-select");
+  const selectedOption = voiceSelect.selectedOptions[0].getAttribute("data-name");
+  for (let i = 0; i < voices.length; i++) {
+    if (voices[i].name === selectedOption) {
+      utterance.voice = voices[i];
+    }
+  }
+  speechSynthesis.speak(utterance);  
+}
+
 function init() {
-  // TODO
+  // populate with list of possible voices
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  // convert to speech
+  let submit_button = document.querySelector('button')
+  submit_button.addEventListener('click', event => speak(event))
 }
